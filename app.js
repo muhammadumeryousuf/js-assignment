2a58849e78ea9cf9c224b5488ccd5bd1fb637b4a
Muhammad umer yousuf <falconhouseschool61@gmail.com>
2024-03-01 08:59:16 -0900

    js assignment

---
+++ app.js
@@ -0,0 +1,79 @@
+// // Qno1
+
+// // var num = 3;
+// // if(num % 3 === 0){
+// //     console.log('number is divisible by 3');
+// // }else{
+// //     console.log('number is not divisible by 3');
+// // }
+// // // Qno2
+// // program to check if the number is even or odd
+// // take input from the user
+// // const number = prompt("enter a number")
+
+// // // check if the number is even
+// // if(number % 2 == 0){
+// //     console.log("the number is even")
+// // }
+// // // if the number is odd
+// // else{
+// //     console.log('the number is odd')
+// // }
+
+
+
+
+
+
+
+// // Qno3
+// // var age = prompt("enter your age");
+
+// // if (age > 18){
+// //     console.log('old enough')
+// // }
+// // else{
+// //     console.log('too young')
+// // }
+
+// // Qno4
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // Qno7
+
+
+
+
+// // Qno11
+// // var userName = prompt("enter your name here")
+// // if(userName === "Fahad"){
+// //     document.write("Hello Fahad!")
+// // };
+// // else{
+// //     document.write("you are not fahad!")
+// // };
+
+// // Qno12
+
+
+// var d = new Date();
+// var t = d.getHours();
+
+// //  var hour = 13;
+//  if (t < 18) {
+//  document.write = ("Good day")};
+//  else{
+//  document.write = ("Good evening");
+//  }
+
